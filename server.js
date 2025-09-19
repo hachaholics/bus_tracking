7727fc411266cc469260ca8e7318c79d32330717
PramodKumar04 <ppramod36963@gmail.com>
2025-09-19 14:24:42 +0530

    chatbot

--- server.js
+++ server.js
@@ -1,23 +1,30 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require("cors");
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 
 // Routes
 const authRoute = require('./routes/authRoute');
 const statusRoute = require('./routes/statusRoute');
 const routeRoutes = require("./routes/routeRoutes");
+const chatBotRoute = require("./routes/chatBotRoute");
+
+app.use("/api/chatbot", chatBotRoute); 
+
 
 app.use("/api/route", routeRoutes);
 app.use('/api/auth', authRoute);
 app.use('/api/status', statusRoute);
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
-  .catch(err => console.error(err));
+// mongoose.connect(process.env.MONGO_URI)
+//   .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+//   .catch(err => console.error(err));
